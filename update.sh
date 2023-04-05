65ae374cfb72391568a6660c24f33aff6fd70daf
ydak <fuzzy.boat0471@fastmail.com>
2023-04-05 23:21:58 +0900

    Update echo msg

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "==================== Start minecraft update ===================="
-echo "Updating ..."
+echo "Updating minecraft ..."
 gcloud compute ssh --zone "us-west1-b" "minecraft" --command="docker pull itzg/minecraft-bedrock-server:latest && docker restart mc-server"
 echo "Minecraft has been updated!"
 echo "==================== End minecraft update ===================="
