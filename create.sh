4811cbe8875f3517d01f9e81828e319f2d8a2eda
ydak <fuzzy.boat0471@fastmail.com>
2023-04-09 00:21:05 +0900

    Modified to allow options to be selected by number.

--- create.sh
+++ create.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+script_dir=$(dirname ${0})
+. $script_dir/functions.sh
+. $script_dir/const.sh
+
 echo "==================== Start create minecraft server  ===================="
 
+# SEED ==========
+cat <<EOS
+-*-*-*-*- [GOOGLE CLOUD INFORMATION (アカウント情報入力)] -*-*-*-*-
+EOS
 echo -n "Project number: "
 read -r project_num
 
@@ -13,88 +21,66 @@ echo -n "Server name (Default: ydak): "
 read -r server_name
 
 # GAME MODE ==========
-cat << EOS
-[GAME MODE]
+cat <<EOS
+
+-*-*-*-*- [GAME MODE (ゲームモード)] -*-*-*-*-
 1. survival (サバイバル)
 2. creative (クリエイティブ)
 3. adventure (アドベンチャー)
 EOS
 echo -n "Select game mode (Default: survival): "
 read -r game_mode_num
-if [ "$game_mode_num" != "" ] && [ "$game_mode_num" != "1" ] && [ "$game_mode_num" != "2" ] && [ "$game_mode_num" != "3" ]; then
-  echo "[ERROR] Enter correct number 1 or 2 or 3."
-  exit 1
-fi
-if [ "$game_mode_num" == "1" ]; then
-  game_mode=survival
-fi
-if [ "$game_mode_num" == "2" ]; then
-  game_mode=creative
-fi
-if [ "$game_mode_num" == "3" ]; then
-  game_mode=adventure
-fi
+if [ "$game_mode_num" == "" ]; then game_mode_num=1 ; fi
+num_validation $game_mode_num 3
+game_mode=${game_mode_list[$game_mode_num-1]}
 
 # DIFFICULTY ==========
-cat << EOS
-[DIFFICULTY]
-1. peaceful (平和)
-2. easy (簡単)
-3. normal (普通)
-4. hard (難しい)
+cat <<EOS
+
+-*-*-*-*- [DIFFICULTY (難易度)] -*-*-*-*-
+1. peaceful (ピースフル)
+2. easy (イージー)
+3. normal (ノーマル)
+4. hard (ハード)
 EOS
 echo -n "Difficulty (Default: normal): "
 read -r difficulty_num
-if [ "$difficulty_num" != "" ] && [ "$difficulty_num" != "1" ] && [ "$difficulty_num" != "2" ] && [ "$difficulty_num" != "3" ] && [ "$difficulty_num" != "4" ]; then
-  echo "[ERROR] Enter correct number 1 or 2 or 3 or 4."
-  exit 1
-fi
-if [ "$difficulty_num" == "1" ]; then
-  difficulty=peaceful
-fi
-if [ "$difficulty_num" == "2" ]; then
-  difficulty=easy
-fi
-if [ "$difficulty_num" == "3" ]; then
-  difficulty=normal
-fi
-if [ "$difficulty_num" == "4" ]; then
-  difficulty=hard
-fi
+if [ "$difficulty_num" == "" ]; then difficulty_num=3 ; fi
+num_validation $difficulty_num 4
+difficulty=${difficulty_list[$difficulty_num-1]}
 
-# ALLOW CHEAT ==========
-cat << EOS
-[ALLOW CHEAT]
-1. yes (有効)
-2. no (無効)
+# CHEAT ==========
+cat <<EOS
+
+-*-*-*-*- [CHEAT (チート)] -*-*-*-*-
+1. ON (有効)
+2. OFF (無効)
 EOS
-echo -n "Allow cheat? (Default: no): "
+echo -n "Allow cheat? (Default: OFF): "
 read -r allow_cheat_num
-if [ "$allow_cheat_num" != "" ] && [ "$allow_cheat_num" != "1" ] && [ "$allow_cheat_num" != "2" ]; then
-  echo "[ERROR] Enter correct number 1 or 2"
-  exit 1
-fi
-if [ "$allow_cheat_num" == "1" ]; then
-  allow_cheat=true
-fi
-if [ "$allow_cheat_num" == "2" ]; then
-  allow_cheat=false
-fi
+if [ "$allow_cheat_num" == "" ]; then allow_cheat_num=2 ; fi
+num_validation $allow_cheat_num 2
+allow_cheat=${allow_cheat_list[$allow_cheat_num-1]}
 
-# ALLOW CHEAT ==========
-cat << EOS
-[ALLOW CHEAT]
+# PERMISSION ==========
+cat <<EOS
+
+-*-*-*-*- [PERMISSION (権限)] -*-*-*-*-
 1. visitor (訪問者)
 2. member (メンバー)
 3. operator (管理者)
 EOS
 echo -n "Default permission (Default: member): "
 read -r permission_num
-if [ "$permission_num" != "" ] && [ "$permission_num" != "1" ] && [ "$permission_num" != "2" ] && [ "$permission_num" != "3" ]; then
-  echo "[ERROR] Enter correct number 1 or 2 or 3"
-  exit 1
-fi
+if [ "$permission_num" == "" ]; then permission_num=2 ; fi
+num_validation $permission_num 3
+permission=${permission_num_list[$permission_num-1]}
 
+# SEED ==========
+cat <<EOS
+
+-*-*-*-*- [SEED (シード値)] -*-*-*-*-
+EOS
 echo -n "Seed (Default: random): "
 read -r seed
 if [ "$seed" != "" ]; then
@@ -152,10 +138,19 @@ docker volume create mc-volume && \
 docker run -d -it --name mc-server --restart=always -e EULA=TRUE -e SERVER_NAME=${server_name:-ydak} -e GAMEMODE=${game_mode:-survival} -e DIFFICULTY=${difficulty:-normal} -e ALLOW_CHEATS=${allow_cheat:-false} -e DEFAULT_PLAYER_PERMISSION_LEVEL=${permission:-member} -e LEVEL_SEED=$seed -p 19132:19132/udp -v mc-volume:/data itzg/minecraft-bedrock-server:latest
 " | jq -r '.[].networkInterfaces[0].accessConfigs[0].natIP')
 
-echo "All Done!! Wait for 3 minutes and access the minecraft!"
-echo ""
-echo "##########################################################################"
-echo "You can access Minecraft using the [$external_ip] server IP address."
-echo "##########################################################################"
-echo ""
+cat
+
+cat <<EOS
+
+All Done!!
+
+Wait for a minute and access the minecraft!
+
+You can access Minecraft using the following IP address!
+
+################################################################################
+${external_ip}
+################################################################################
+
+EOS
 echo "==================== End create minecraft server  ===================="
