4055a4e1688c886c8118b1d2dc9d70c69c073e26
ydak <62167854+ydak@users.noreply.github.com>
2023-04-10 00:33:56 +0900

    Add auto gcp info (#6)

--- create.sh
+++ create.sh
@@ -7,28 +7,40 @@ script_dir=$(dirname ${0})
 
 echo "==================== Start create minecraft server  ===================="
 
-# SEED ==========
-cat <<EOS
--*-*-*-*- [GOOGLE CLOUD INFORMATION (アカウント情報入力)] -*-*-*-*-
-EOS
-echo -n "Project number: "
-read -r project_num
-
-echo -n "Project id: "
-read -r project_id
-
+# GOOGLE CLOUD ==========
+echo -n "Setting Google Cloud info ..."
+project_id=$(gcloud config get project)
+project_num=$(gcloud projects list --filter="$project_id" --format="value(PROJECT_NUMBER)")
 gcloud config set project $project_id
 
+cat <<EOS
+
+-*-*-*-*- [GOOGLE CLOUD (Google Cloud の情報確認)] -*-*-*-*-
+プロジェクト ID  : $project_id
+プロジェクト番号 : $project_num
+
+上記の Google Cloud 環境でマインクラフトサーバーを作成します。
+EOS
+
+echo -n "よろしいですか? [y/N]: "
+read -r gcp_info
+if [ "$gcp_info" != "y" ]; then exit 1 ; fi
+
+# SERVER NAME ==========
+cat <<EOS
+
+-*-*-*-*- [SERVER NAME (マインクラフトサーバー名を自由に決めて下さい)] -*-*-*-*-
+EOS
 echo -n "Server name (Default: ydak): "
 read -r server_name
 
 # GAME MODE ==========
 cat <<EOS
 
--*-*-*-*- [GAME MODE (ゲームモード)] -*-*-*-*-
-1. survival (サバイバル)
-2. creative (クリエイティブ)
-3. adventure (アドベンチャー)
+-*-*-*-*- [GAME MODE (ゲームモードを選択)] -*-*-*-*-
+[1] survival (サバイバル)
+[2] creative (クリエイティブ)
+[3] adventure (アドベンチャー)
 EOS
 echo -n "Select game mode (Default: survival): "
 read -r game_mode_num
@@ -39,11 +51,11 @@ game_mode=${game_mode_list[$game_mode_num-1]}
 # DIFFICULTY ==========
 cat <<EOS
 
--*-*-*-*- [DIFFICULTY (難易度)] -*-*-*-*-
-1. peaceful (ピースフル)
-2. easy (イージー)
-3. normal (ノーマル)
-4. hard (ハード)
+-*-*-*-*- [DIFFICULTY (難易度を選択)] -*-*-*-*-
+[1] peaceful (ピースフル)
+[2] easy (イージー)
+[3] normal (ノーマル)
+[4] hard (ハード)
 EOS
 echo -n "Difficulty (Default: normal): "
 read -r difficulty_num
@@ -54,9 +66,9 @@ difficulty=${difficulty_list[$difficulty_num-1]}
 # CHEAT ==========
 cat <<EOS
 
--*-*-*-*- [CHEAT (チート)] -*-*-*-*-
-1. ON (有効)
-2. OFF (無効)
+-*-*-*-*- [CHEAT (チートを有効にするかどうか)] -*-*-*-*-
+[1] ON (有効)
+[2] OFF (無効)
 EOS
 echo -n "Allow cheat? (Default: OFF): "
 read -r allow_cheat_num
@@ -67,10 +79,10 @@ allow_cheat=${allow_cheat_list[$allow_cheat_num-1]}
 # PERMISSION ==========
 cat <<EOS
 
--*-*-*-*- [PERMISSION (権限)] -*-*-*-*-
-1. visitor (訪問者)
-2. member (メンバー)
-3. operator (管理者)
+-*-*-*-*- [PERMISSION (サーバーに参加するユーザー全員の権限)] -*-*-*-*-
+[1] visitor (訪問者)
+[2] member (メンバー)
+[3] operator (管理者)
 EOS
 echo -n "Default permission (Default: member): "
 read -r permission_num
@@ -81,7 +93,7 @@ permission=${permission_num_list[$permission_num-1]}
 # SEED ==========
 cat <<EOS
 
--*-*-*-*- [SEED (シード値)] -*-*-*-*-
+-*-*-*-*- [SEED (シード値を入力。入力しない場合はランダム)] -*-*-*-*-
 EOS
 echo -n "Seed (Default: random): "
 read -r seed
@@ -93,6 +105,7 @@ if [ "$seed" != "" ]; then
 fi
 
 echo "Configuration is OK. The next step is to create a minecraft server."
+echo "(サーバー設定が完了しました。マインクラフトサーバーの作成を開始します。)"
 
 echo "Enabling compute.googleapis.com ..."
 gcloud services enable compute.googleapis.com
@@ -114,14 +127,14 @@ if [ -z "$fw_minecraft" ]; then
     --source-ranges=0.0.0.0/0 \
     --target-tags=minecraft
 fi
-echo "Firewall creation done."
+echo "Firewall creation complete!"
 
 # GCE ==========================================================================
 echo "Checking latest COS image ..."
 image=$(gcloud compute images list --format="json" | jq -r '.[] | select(.family | test("cos-stable")) | .selfLink' | sed -E 's/.*(projects.*)/\1/')
-echo "COS image Check Done."
+echo "COS image check complete."
 
-echo "Creating GCE for minecraft ..."
+echo "Creating server for minecraft ..."
 
 external_ip=$(gcloud compute instances create minecraft \
   --format="json" \
@@ -143,13 +156,18 @@ docker volume create mc-volume && \
 docker run -d -it --name mc-server --restart=always -e EULA=TRUE -e SERVER_NAME=${server_name:-ydak} -e GAMEMODE=${game_mode:-survival} -e DIFFICULTY=${difficulty:-normal} -e ALLOW_CHEATS=${allow_cheat:-false} -e DEFAULT_PLAYER_PERMISSION_LEVEL=${permission:-member} -e LEVEL_SEED=$seed -p 19132:19132/udp -v mc-volume:/data itzg/minecraft-bedrock-server:latest
 " | jq -r '.[].networkInterfaces[0].accessConfigs[0].natIP')
 
+echo "Creating server complete!"
+
 cat <<EOS
 
 All Done!!
+ (すべて完了しました！！)
 
 Wait for a minute and access the minecraft!
+ (数分後にマインクラフトへアクセスできます)
 
 You can access Minecraft using the following IP address!
+(下記のIPアドレスを使用してマインクラフトにアクセス可能です！)
 
 ################################################################################
 ${external_ip}
